02bec082b72437032c07d30cbd77a85051c6d933
Alex Astgen <alexastgen@gmail.com>
2016-11-29 16:16:44 -0500

    Add files via upload

---
+++ io/ninelives/blog/alex/graphics/Start.java
@@ -0,0 +1,17 @@
+package io.ninelives.blog.alex.graphics;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import io.ninelives.blog.alex.graphics.gfx.Bitmap;
+
+public class Start {
+
+	public static void main(String[] args) throws IOException {
+		Bitmap bmp = new Bitmap("C:/Users/Alex Astgen/Downloads/images.jpg");
+	}
+}
