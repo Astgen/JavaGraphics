be826199a552af0dd3681794f2138ed186507b62
Alex Astgen <alexastgen@gmail.com>
2016-11-30 20:11:51 -0500

    Add files via upload

--- io/ninelives/blog/alex/graphics/Start.java
+++ io/ninelives/blog/alex/graphics/Start.java
@@ -1,17 +1,26 @@
 package io.ninelives.blog.alex.graphics;
 
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.awt.Graphics2D;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
 import io.ninelives.blog.alex.graphics.gfx.Bitmap;
 
 public class Start {
+	
+	static Graphics2D graphics;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		JFrame frame = new JFrame("Graphics Guide");
+		frame.setSize(640,480);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		graphics = (Graphics2D) frame.getContentPane().getGraphics();
+		
 		Bitmap bmp = new Bitmap("C:/Users/Alex Astgen/Downloads/images.jpg");
+
+
+		bmp.draw(0,0,graphics);
 	}
 }
