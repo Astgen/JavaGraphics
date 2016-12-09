767f43c3a609873635d19c8810dea3e3d544f884
Alex Astgen <alexastgen@gmail.com>
2016-12-09 14:33:07 -0500

    Update Start.java

--- io/ninelives/blog/alex/graphics/Start.java
+++ io/ninelives/blog/alex/graphics/Start.java
@@ -13,28 +13,15 @@ public class Start {
 	
 	static Graphics2D graphics;
 
-	static Bitmap bmp = new Bitmap("C:/Users/Alex Astgen/Downloads/images.jpeg");
+	static Bitmap bmp = new Bitmap("Path/To/Image.jpg");
 
 	public static void main(String[] args) throws Exception {
-		JFrame frame = new JFrame("Graphics Guide"){
-			@Override
-			public void paint(Graphics g){
-				BufferedImage bi = new BufferedImage(getContentPane().getWidth() + 50, getContentPane().getHeight() + 50, BufferedImage.TYPE_INT_ARGB);
-				Start.graphics = (Graphics2D) bi.getGraphics();
-				graphics.fillRect(0, 0, bi.getWidth(), bi.getHeight());
-				Start.bmp.draw(0, 0, graphics);
-				g.drawImage(bi, 0, 0, null);
-				//bmp.gaussianBlur().draw(bmp.getWidth(), 0, graphics);
-				//bmp.edgeDetect().draw(0, bmp.getHeight(), graphics);
-				//bmp.emboss().draw(bmp.getWidth(), bmp.getHeight(), graphics);
-			}};
+		JFrame frame = new JFrame("Graphics Guide");
 		frame.setSize(640,480);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		graphics = (Graphics2D) frame.getContentPane().getGraphics();
 
-		while(true){
-			frame.repaint();
-		}
+		bmp.emboss().draw(0,0, graphics);
 	}
 }
